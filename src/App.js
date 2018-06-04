67ce3d1041f100ad05a16bd846843f43981d7036
uttam <uttamkumar.rapeti@capgemini.com>
2018-06-04 13:10:44 +0530

    Commit Three with react routing

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import CategoryList from "./components/CategoryList";
 import Footer from "./components/Footer";
+import productsData from "./data/products";
 
 class App extends Component {
   render() {
+      console.log(this.props.match.params)
     return (
       <div className="container-fluid p-0">
         <Header />
         <section>
           <div className="row">
             <div className="col-sm-3">
-              <Sidebar />
+              <Sidebar data={productsData} />
+                <h2>Uttam</h2>
             </div>
             <div className="col-sm-9">
               <CategoryList />
